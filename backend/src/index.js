6b2b20a8ab41614cae18b99e3dda36ba5e4f1ab1
Elderick <erickoliveiraah@gmail.com>
2020-01-18 02:03:22 -0300

    Front-end done

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors')
 const routes = require('./routes')
 
 const app = express()
@@ -8,6 +9,7 @@ mongoose.connect('mongodb+srv://elderick:fullstack@fullstack-yajra.gcp.mongodb.n
    useNewUrlParser: true, useUnifiedTopology: true 
 })
 
+app.use(cors())
 app.use(express.json()) //Diz para o express entender JSON
 app.use(routes)
 
